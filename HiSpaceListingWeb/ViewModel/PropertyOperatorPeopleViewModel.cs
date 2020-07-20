5413d43c93bd2a5531d6d669067a35953322f05a
Kanu Priya <kanu.priya@highbrowdiligence.com>
2020-07-20 15:41:26 +0530

    Adding PropertyOperatorPeople ViewModel

---
+++ HiSpaceListingWeb/ViewModel/PropertyOperatorPeopleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HiSpaceListingModels;
+using HiSpaceListingService.ViewModel;
+
+namespace HiSpaceListingWeb.ViewModel
+{
+    public class PropertyOperatorPeopleViewModel
+    {
+        public PropertyOperatorPeopleViewModel()
+        {
+            List<Listing> Listings = new List<Listing>();
+            List<PropertyOperatorResponse> Operators = new List<PropertyOperatorResponse>();
+            List<PropertyPeopleResponse> People = new List<PropertyPeopleResponse>();
+        }
+
+        public List<Listing> Listings { get; set; }
+        public List<PropertyOperatorResponse> Operators { get; set; }
+        public List<PropertyPeopleResponse> People { get; set; }
+    }
+}
